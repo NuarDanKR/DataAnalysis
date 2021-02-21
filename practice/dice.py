1e64d708e928845a67ad3e3abca248d80a7bb53f
NuarDAN <nuardankr@gmail.com>
2021-02-21 23:46:06 +0900

    Mod dice.py, Create list_practice.py

--- practice/dice.py
+++ practice/dice.py
@@ -8,12 +8,12 @@ if not(0<val<7):
     print("잘못된 값을 입력하셨어요")
     val = int(input("값 :"))
     if dice==val:
-            print("정답입니다!",dice)
-            break
+        print("정답입니다!",dice)
+        break
 
 elif dice==val:
     print("정답입니다!",dice)
-    
+
 else:
     while dice!=val:
         print("틀렸습니다 값을 다시 입력해주세요.",dice)
---
+++ practice/list_practice.py
@@ -0,0 +1,12 @@
+
+#%%
+a=[2,8,0,5,7]
+print("a:",a)
+b=a.copy()
+print("b:",b)
+
+b.append(5)
+print("b.append:",b)
+
+c=list(range(0,20,2))
+print("c:",c)
