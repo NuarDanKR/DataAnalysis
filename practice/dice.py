7db9a54d6d72c117bd5efb425cbc046bf5e9eb9f
NuarDAN <nuardankr@gmail.com>
2021-02-21 22:39:18 +0900

    Del random.py, Update dice.py

--- practice/dice.py
+++ practice/dice.py
@@ -1,5 +1,23 @@
 import random as rand
 
+dice = rand.randint(1,6)
 
-dice=[1,2,3,4,5,6]
-print(rand.choice(dice))
+val = int(input("값 :"))
+
+if not(0<val<7):
+    print("잘못된 값을 입력하셨어요")
+    val = int(input("값 :"))
+    if dice==val:
+            print("정답입니다!",dice)
+            break
+
+elif dice==val:
+    print("정답입니다!",dice)
+    
+else:
+    while dice!=val:
+        print("틀렸습니다 값을 다시 입력해주세요.",dice)
+        val = int(input("값 :"))
+        if dice==val:
+            print("정답입니다!",dice)
+            break
