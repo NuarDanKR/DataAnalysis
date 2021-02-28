8797675d46cde4b85f31539f1892e80716468e4e
NuarDAN <nuardankr@gmail.com>
2021-03-01 00:25:25 +0900

    Create many things..

---
+++ practice/recursive.py
@@ -0,0 +1,13 @@
+def is_palindrome(word):
+    if len(word)<2:
+        return True
+    if word[0]!=word[-1]:
+        return False
+    else:
+        return is_palindrome(word[1:-1])
+
+
+
+
+    
+
